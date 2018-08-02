95674de99fa2edde4bea266a2acd3c3c037940ef
murtaza98 <murtazaforuall@yahoo.in>
2018-08-02 12:22:21 +0530

    Register backend

--- register.php
+++ register.php
@@ -9,20 +9,42 @@
 <?php include "./templates/navigation.php"; ?>
 
 
+<?php
+    if(isset($_POST['submit'])){
+        $user_name = $_POST['username'];
+        $user_pass = $_POST['password'];
+        $user_firstName = $_POST['firstname'];
+        $user_lastname = $_POST['lastname'];
+        $user_address = $_POST['address'];
+        $user_contact = $_POST['contact'];
+        $user_role = "user";
+        
+        $query = "INSERT INTO users(username,password,first_name,last_name,role,area,contact_no) VALUES('$user_name','$user_pass','$user_firstName','$user_lastname','$user_role','$user_address',$user_contact)";
+        
+        $query_result = mysqli_query($connection,$query);
+        
+        if(!$query_result){
+            die("QUERY FAILED ".mysqli_error($connection));
+        }else{
+            echo "<h2 class='text-center text-success'><b>DATA SAVED</b></h2>";
+        }
+        
+    }
+?>
 <div class="container">
     <h1 class="text-center">Registration Form</h1>
-    <form>
+    <form method="post" action="./register.php">
         Email Id:<br>
         <input type="Email" name="username" size="46"> <br><br>
         Password:<br>
         <input type="Password" name="password" size="46"> <br><br>
         Confirm Password: <br>
-        <input type="P" name="confirmpwd" size="46"> <br><br>
+        <input type="Password" name="confirmpwd" size="46"> <br><br>
         First Name: &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; Last Name:<br>
         <input type="text" style="width: 187px;" name="firstname"> &nbsp;
         <input type="text" style="width: 186px;" name="lastname"> <br><br>
         Address:<br>
-        <textarea style="width: 382px; height: 78px;"></textarea> <br><br>
+        <textarea name="address" style="width: 382px; height: 78px;"></textarea> <br><br>
         Contact number:<br>
         <input type="text" name="contact" size="46"> <br><br>
 
--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,4 @@
+<?php include "./templates/db.php"; ?>
 <!DOCTYPE html>
 <html>
 <head>
