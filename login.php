e35789fcc9ea56d4d3a02c36020c999d15ec20f8
murtaza98 <murtazaforuall@yahoo.in>
2018-08-03 11:22:51 +0530

    Login Functionality added

--- login.php
+++ login.php
@@ -8,11 +8,34 @@
 
 <?php include "./templates/navigation.php"; ?>
 
+<?php
+    if(isset($_POST['submit'])){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        
+        $query = "SELECT * FROM users WHERE username = '{$username}' AND password = '{$password}'";
+        
+        $query_result = mysqli_query($connection,$query);
+        
+        if(!$query_result){
+            die("FAILED" . mysqli_error($connection));
+        }else{
+            $row_count = mysqli_num_rows($query_result);
+            if($row_count == 1){
+                while($row = mysqli_fetch_assoc($query_result)){
+                    $name = $row['first_name'];
+                    echo "<h2 class = 'text-center'>WELCOME {$name}</h2>"; 
+                }
+            }
+        }
+    }
+?>
+
 <div class="container">
   <h1>LOGIN</h1>
-  <form>
+  <form method="post" action="./login.php">
     Email Id:<br>
-    <input type="Email" name="username"> <br><br>
+    <input type="text" name="username"> <br><br>
     Password:<br>
     <input type="Password" name="password"> <br><br>
     
