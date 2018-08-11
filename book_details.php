faf9edddc9a8943f1017c1fabe8b8a71c726a3b6
ojasskapre <ojas.kapre@somaiya.edu>
2018-08-11 19:01:12 +0530

    modified bookDetails page

--- book_details.php
+++ book_details.php
@@ -12,12 +12,23 @@
 	<div class="row">
 		<div class="col-sm-4" style="position: sticky; top: 0;">
 			<img src="includes/images/coa.jpg">
-			<button type="button" class="btn btn-primary">Bookmark</button>
-			<button type="button" class="btn btn-primary">Buy Now</button>
+			<button type="button" class="btn" style="width: 49.3%; background-color: #396a94; color: white">Bookmark</button>
+			<button type="button" class="btn" style="width: 49.3%; background-color: #18456b; color: white">Buy Now</button>
 		</div>
 		<div class="col-sm-6">
-			<h3>Computer Organization and Architecture</h3>
-			<h4>by William Stalling</h4>
+			<div class="bookName">
+				<h3>Computer Organization and Architecture</h3>
+				<h4>by William Stalling</h4>
+			</div>
+		<div class="price">
+			<span style="font-size: 28px;">₹350 </span>
+			<span style="font-size: 16px; color: #878787; text-decoration: line-through;">₹600 </span>
+		</div>
+		<div class="seller">
+			<h5><strong>Seller's information</strong></h5>
+		</div>
+		<div class="bookDesc">
+			<h5><strong>Book Description</strong></h5>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -54,17 +65,6 @@
 			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
 	</div>
 </div>
