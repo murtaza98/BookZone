5fda9786510319e86a6f7b2602634bd3db6bf2b5
murtaza98 <asdasdasd@fna.as>
2018-08-23 12:25:14 +0530

    added View_all_users

--- admin/templates/admin-navigation.php
+++ admin/templates/admin-navigation.php
@@ -72,7 +72,7 @@
             <a href="javascript:;" data-toggle="collapse" data-target="#users_toggle"><i class="fa fa-fw fa-arrows-v"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
             <ul id="users_toggle" class="collapse">
                 <li>
-                    <a href="#">View All Users</a>
+                    <a href="users.php">View All Users</a>
                 </li>
                 <li>
                     <a href="#">Add User</a>
--- admin/users.php
+++ admin/users.php
@@ -1,13 +1,82 @@
 <?php
     function customPageHeader(){
-        // echo "<link rel='stylesheet' type='text/css' href='includes/css/index.css'>";
+    	 echo "<link rel='stylesheet' type='text/css' href='includes/css/users.css'>";
     }
 ?>
 <?php include "./templates/admin-header.php"; ?>
 
 <?php include "./templates/admin-navigation.php"; ?>
 
+<div id="wrapper">
+	<div id="page-wrapper">
+		<div class="container-fluid">
+			<h3 class="text-center"><b><u>ALL USERS</u></b></h3>
+			<div class="row">
+				<div class="col-lg-12">
+					<table class="table table-bordered table-hover">
+						<thead>
+							<tr>
+								<th class="text-center">UserName</th>
+								<th class="text-center">Email</th>
+								<th class="text-center">Name</th>
+								<th class="text-center">Role</th>
+								<th class="text-center">City</th>
+								<th class="text-center">Pincode</th>
+								<th class="text-center">Contact</th>
+								<th class="text-center">User Category</th>
+								<th class="text-center">Edit</th>
+								<th class="text-center">Delete</th>
+							</tr>
+						</thead>
+						<tbody>
 
+							<?php
+								$query = "SELECT * from users";
+
+								$query_result = mysqli_query($connection,$query);
+
+								if(!$query_result){
+									die("QUERY FAILED " .mysqli_error($connection));
+								}else{
+									while ($row = mysqli_fetch_assoc($query_result)) {
+										$username = $row['username'];
+										$email = $row['email'];
+										$name = $row['first_name'] . $row['middle_name'] .$row['last_name'];
+										$role = $row['role'];
+										$city = $row['city'];
+										$pincode = $row['pincode'];
+										$contact = 1234456;
+										$user_category = $row['user_category'];
+							?>
+
+										<tr>
+											<td class="text-center"><?php echo $username ?></td>
+											<td class="text-center"><?php echo $email ?></td>
+											<td class="text-center"<?php echo $name ?></td>
+											<td class="text-center"><?php echo $role ?></td>
+											<td class="text-center"><?php echo $city ?></td>
+											<td class="text-center"><?php echo $pincode ?></td>
+											<td class="text-center"<?php echo $contact ?></td>
+											<td class="text-center"><?php echo $user_category ?></td>
+											<td class="text-center"><a href="#" class="btn btn-primary">Edit</a></td>
+											<td class="text-center"><a href="#" class="btn btn-danger">Delete</a></td>
+										</tr>
+
+
+							<?php
+									}
+
+								}
+
+							?>
+						</tbody>						
+					</table>
+				</div>	
+			</div>	
+		</div>
+	</div>
+
+</div>
 
 
 <?php include "templates/admin-footer.php"; ?>
