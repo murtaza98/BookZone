62359fc2a5f10cd492bfd32aae24e411753db4cb
Murtaza Patrawala <murtazaforuall@yahoo.in>
2018-09-30 01:23:51 +0530

    search backend and frontend linked

--- index.php
+++ index.php
@@ -13,6 +13,9 @@
 <div class= "container close_bookmark_sidebar" id="page-content-wrapper" style="margin: 0px; padding: 0px">
 <div class="container-fluid row" style=" background-image:url('includes/images/image1.jpg'); background-repeat: no-repeat; background-size:100% 100%; height: 400px;">
 
+<br>
+<br>
+
 <p align="center" style="color: white; font-size: 30px;font-family: fantasy;">Buy and Sell Books Online</p>
 
 
@@ -20,7 +23,7 @@
 <form method="GET" action="">
     <div class="container-fluid close_bookmark_sidebar autocomplete"  style="background-color: transparent; margin-top: 20px;">
         <div class="full-width-util input-group" id="searchBox">
-            <input id="myInput" type="text" name="search_box"  class="form-control home-search-bar acInput" placeholder="Search by title, author, semester" autocomplete="off" style="width:100%;">
+            <input id="myInput" type="text" name="search_box"  class="form-control home-search-bar acInput" placeholder="Search by title, author, semester" autocomplete="off" style="width:100%; background-color:#fff">
             <span class="input-group-btn">
                 <button type="submit" value="Search" id="searchButtonInline" class="btn btn-primary no-top-margin">Go!</button>
             </span>
@@ -36,40 +39,14 @@
   <span>
       <input type="submit">
   </span>
-  
 </form>
 -->
 <!-- search box end -->
 
-<div id="livesearch"></div>
-<script>
-//function showResult(str) {
-//  if (str.length==0) { 
-//    document.getElementById("livesearch").innerHTML="";
-//    document.getElementById("livesearch").style.border="0px";
-//    return;
-//  }
-//  if (window.XMLHttpRequest) {
-//    // code for IE7+, Firefox, Chrome, Opera, Safari
-//    xmlhttp=new XMLHttpRequest();
-//  } else {  // code for IE6, IE5
-//    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-//  }
-//  xmlhttp.onreadystatechange=function() {
-//    if (this.readyState==4 && this.status==200) {
-//      document.getElementById("livesearch").innerHTML=this.responseText;
-//      document.getElementById("livesearch").style.border="1px solid #A5ACB2";
-//    }
-//  }
-//  xmlhttp.open("GET","templates/livesearch.php?q="+str,true);
-//  xmlhttp.send();
-//}
-</script>
-
 <script>
     var countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
 
-    autocomplete(document.getElementById("myInput"),document.getElementById("parent_element"), countries);
+    autocomplete(document.getElementById("myInput"),document.getElementById("parent_element"));
     
 </script>
 
--- templates/livesearch.php
+++ templates/livesearch.php
@@ -1,56 +1,82 @@
 <?php
-// Array with names
-$a[] = "Anna";
-$a[] = "Brittany";
-$a[] = "Cinderella";
-$a[] = "Diana";
-$a[] = "Eva";
-$a[] = "Fiona";
-$a[] = "Gunda";
-$a[] = "Hege";
-$a[] = "Inga";
-$a[] = "Johanna";
-$a[] = "Kitty";
-$a[] = "Linda";
-$a[] = "Nina";
-$a[] = "Ophelia";
-$a[] = "Petunia";
-$a[] = "Amanda";
-$a[] = "Raquel";
-$a[] = "Cindy";
-$a[] = "Doris";
-$a[] = "Eve";
-$a[] = "Evita";
-$a[] = "Sunniva";
-$a[] = "Tove";
-$a[] = "Unni";
-$a[] = "Violet";
-$a[] = "Liza";
-$a[] = "Elizabeth";
-$a[] = "Ellen";
-$a[] = "Wenche";
-$a[] = "Vicky";
 
-// get the q parameter from URL
-$q = $_REQUEST["q"];
 
-$hint = "";
+$countries = array("Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe");
 
-// lookup all hints from array if $q is different from "" 
-if ($q !== "") {
-    $q = strtolower($q);
-    $len=strlen($q);
-    foreach($a as $name) {
-        if (stristr($q, substr($name, 0, $len))) {
-            if ($hint === "") {
-                $hint = $name;
-            } else {
-                $hint .= ", $name";
-            }
-        }
-    }
-}
+$myJSON = json_encode($countries);
+echo $myJSON;
 
-// Output "no suggestion" if no hint was found or output correct values 
-echo $hint === "" ? "no suggestion" : $hint;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//// Array with names
+//$a[] = "Anna";
+//$a[] = "Brittany";
+//$a[] = "Cinderella";
+//$a[] = "Diana";
+//$a[] = "Eva";
+//$a[] = "Fiona";
+//$a[] = "Gunda";
+//$a[] = "Hege";
+//$a[] = "Inga";
+//$a[] = "Johanna";
+//$a[] = "Kitty";
+//$a[] = "Linda";
+//$a[] = "Nina";
+//$a[] = "Ophelia";
+//$a[] = "Petunia";
+//$a[] = "Amanda";
+//$a[] = "Raquel";
+//$a[] = "Cindy";
+//$a[] = "Doris";
+//$a[] = "Eve";
+//$a[] = "Evita";
+//$a[] = "Sunniva";
+//$a[] = "Tove";
+//$a[] = "Unni";
+//$a[] = "Violet";
+//$a[] = "Liza";
+//$a[] = "Elizabeth";
+//$a[] = "Ellen";
+//$a[] = "Wenche";
+//$a[] = "Vicky";
+//
+//// get the q parameter from URL
+//$q = $_REQUEST["q"];
+//
+//$hint = "";
+//
+//// lookup all hints from array if $q is different from "" 
+//if ($q !== "") {
+//    $q = strtolower($q);
+//    $len=strlen($q);
+//    foreach($a as $name) {
+//        if (stristr($q, substr($name, 0, $len))) {
+//            if ($hint === "") {
+//                $hint = $name;
+//            } else {
+//                $hint .= ", $name";
+//            }
+//        }
+//    }
+//}
+//
+//// Output "no suggestion" if no hint was found or output correct values 
+//echo $hint === "" ? "no suggestion" : $hint;
 ?>
