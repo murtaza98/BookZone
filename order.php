29d4f73354d02042b671d2cdd728e7dd7a30c1bd
Murtaza Patrawala <murtazaforuall@yahoo.in>
2018-10-05 23:45:37 +0530

    bug fixes order

--- order.php
+++ order.php
@@ -1,51 +1,50 @@
-<?php
-    function customPageHeader(){
-    }
-?>
-<?php include "./templates/header.php"; ?>
+<?php ob_start(); ?>
+<?php session_start(); ?>
+<?php include "./templates/db.php"; ?>
 <?php 
 	if (isset($_GET['notification_id']) && isset($_GET['type']) && isset($_SESSION['username'])) {
 		$notification_id = $_GET['notification_id'];
 		$type = $_GET['type'];
-		$query = "SELECT * FROM notification WHERE notification_id = {$notification_id}";
+		$query = "SELECT * FROM notification WHERE notification_id = ".$notification_id;
 		$query_result = mysqli_query($connection,$query);
 		if(!$query_result){
             die("QUERY FAILED ".mysqli_error($connection));
         }else{
 			$row =  mysqli_fetch_assoc($query_result);
-			$buyer_username = $row['buyer_username'];
-			$seller_username = $row['seller_username'];
+			$buyer_username = $row['buyer_name'];
+			$seller_username = $row['username'];
 		}
 		switch($type){
             case "acceptOrder":
-                $query = "UPDATE notification SET order_status = 'accepted' WHERE notification_id = {notification_id}";
-                $message = "'{$seller_username}' has accepted your order";
-                $reply_query = "INSERT INTO notification VALUES(NULL,'$buyer_username','$message','Unseen','$seller_username','$date','reply')";
+                $query = "UPDATE notification SET offer_status = 'accepted' WHERE notification_id = {$notification_id}";
+                $message = "{$seller_username} has accepted your order";
+                $reply_query = "INSERT INTO notification(username,message,status,buyer_name,offer_status,date) VALUES('$buyer_username','$message','Unseen','$seller_username','reply',now())";
                 break;
             case "declineOrder":
-                $query = "UPDATE notification SET order_status = 'rejected' WHERE notification_id = {notification_id}";
-                $message = "'{$seller_username}' has rejected your order";
-                $reply_query = "INSERT INTO notification VALUES(NULL,'$buyer_username','$message','Unseen','$seller_username','$date','reply')";
+                $query = "UPDATE notification SET offer_status = 'rejected' WHERE notification_id = {$notification_id}";
+                $message = "{$seller_username} has rejected your order";
+                $reply_query = "INSERT INTO notification(username,message,status,buyer_name,offer_status,date) VALUES('$buyer_username','$message','Unseen','$seller_username','reply',now())";
                 break;
             default:
                 $query = null;
+                $reply_query = null;
                 break;
         }
         
         
-        if($query != null){
+        if($query != null && $reply_query != null){
             $query_result = mysqli_query($connection,$query); 
             if(!$query_result){
-                die("<script>
-                        window.location.href = 'notification.php';
-                        window.alert('Already bookmarked');
-                      </script>");
+                die("QUERY FAILED ".mysqli_error($connection));
             }else{
-                echo "<script>
-                        window.location.href = 'notification.php';
-                        window.alert('Added to bookmark');
-                      </script>";
-            }   
+                echo "update success";
+            }
+            $query_result = mysqli_query($connection,$reply_query); 
+            if(!$query_result){
+                die("QUERY FAILED ".mysqli_error($connection));
+            }else{
+                echo "Reply success";
+            }
         }
 	}
 ?>
