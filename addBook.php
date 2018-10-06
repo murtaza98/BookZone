0315d1e75353c4e927d6a10480f2df23c8892957
Murtaza Patrawala <murtazaforuall@yahoo.in>
2018-10-06 16:15:41 +0530

    add image functionality added

--- addBook.php
+++ addBook.php
@@ -27,87 +27,138 @@
         $book_subject = $_POST['book_subject'];
         
         $seller_name = $_SESSION['username'];
-
-		$query = "INSERT INTO books(username,book_name,author,edition,subject,category_id,book_price,book_original_price,book_description,date) ";
-        $query .= "VALUES('{$seller_name}','{$book_name}','{$book_author}','{$book_edition}','{$book_subject}',{$book_category},{$book_price},{$book_original_price},'{$book_description}',now())";
         
         
-        $query_result = mysqli_query($connection,$query);
+        $post_image = $_FILES['post_image']['name'];
+		$post_image_temp_location = $_FILES['post_image']['tmp_name'];
+		$post_image_error_code = $_FILES['post_image']['error'];
         
-        if(!$query_result){
-            die('QUERY FAILED '.mysqli_error($connection));
+        $final_image_name = $book_name . '_' . $seller_name . '_'. rand(1,100) . '.' .  pathinfo($post_image)['extension'];        
+        
+        
+        if($post_image_error_code === 0){
+			move_uploaded_file($post_image_temp_location,"./includes/images/$final_image_name");
+            
+            $query = "INSERT INTO books(username,book_name,author,edition,subject,category_id,book_price,book_original_price,book_description,date,book_image) ";
+            $query .= "VALUES('{$seller_name}','{$book_name}','{$book_author}','{$book_edition}','{$book_subject}',{$book_category},{$book_price},{$book_original_price},'{$book_description}',now(),'{$final_image_name}')";
+            
+            $query_result = mysqli_query($connection,$query);
+        
+            if(!$query_result){
+                die('QUERY FAILED '.mysqli_error($connection));
+            }else{
+                $book_id = mysqli_insert_id($connection);
+                $_SESSION['inter_page_message'] = '<h4 class="text-center text-success">Book Added....Click <a href="book_details.php?book_id='.$book_id.'">here</a> to view the book</h4>';
+                
+                header('Location: addBook.php');
+            }
+            
+            
         }else{
-            echo "<h2>Book Added</h2>";
+            $_SESSION['inter_page_message'] = '<h3 class="text-center text-danger">Error Uploading Image</h3>';
+            header('Location: addBook.php');
         }
+        
+        
+        
+
+//		$query = "INSERT INTO books(username,book_name,author,edition,subject,category_id,book_price,book_original_price,book_description,date) ";
+//        $query .= "VALUES('{$seller_name}','{$book_name}','{$book_author}','{$book_edition}','{$book_subject}',{$book_category},{$book_price},{$book_original_price},'{$book_description}',now())";
+        
+        
+        
 	}
+?>
+
+<?php
+    if(isset($_SESSION['inter_page_message'])){
+        echo $_SESSION['inter_page_message'];
+        $_SESSION['inter_page_message'] = null;
+    }
+
 ?>
 
 <h3 class="text-center text-success"><b><u>Tell us more about your book</u></b></h3>
 <br>
 <br>
 <div class="container">
-	<form method="POST" action="">
+	<form method="POST" action="" enctype="multipart/form-data">
 		<div class="row">
 			<div class="col-lg-6">
-				Book Name <br>
-				<div class="input-group">
-                    <span class="input-group-addon"><i class="fa fa-book"></i></span>
-					<input name="book_name" type="text" class="form-control" placeholder="Enter book name" required="true">
-				</div>  <br><br>
-			  	Price <br>
-				<div class="input-group">
-                    <span class="input-group-addon"><i class="fa fa-inr"></i></span>
-                    <input name="book_price" type="text" class="form-control" placeholder="Enter price" required="true">
-                </div> 	 <br><br>
-                Subject <br>
-				<div class="input-group">
-                    <span class="input-group-addon"><i class="fa fa-inr"></i></span>
-                    <input name="book_subject" type="text" class="form-control" placeholder="Enter Subject" required="true">
-                </div> 	 <br><br>
-				Edition <br>
-				<input name="book_edition" type="text" class="form-control" placeholder="Enter book edition" required="true">
-				
+			    <div class="form-group">
+                    <label for="book_category">Book Name</label>
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="fa fa-book"></i></span>
+                        <input name="book_name" type="text" class="form-control" placeholder="Enter book name" required="true">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="book_category">Selling Price</label>
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="fa fa-inr"></i></span>
+                        <input name="book_price" type="text" class="form-control" placeholder="Enter price" required="true">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="book_category">Subject</label>
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="fa fa-inr"></i></span>
+                        <input name="book_subject" type="text" class="form-control" placeholder="Enter Subject" required="true">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="book_category">Edition</label>
+				    <input name="book_edition" type="text" class="form-control" placeholder="Enter book edition" required="true">
+                </div>
 			</div>
 			<div class="col-lg-6">
-				Author <br>
-				<div class="input-group">
-                    <span class="input-group-addon"><i class="fa fa-user"></i></span>	
-					<input name="book_author" type="text" class="form-control" placeholder="Enter author name" required="true"> 
-				</div> <br><br>
-				Original Price <br>
-				<div class="input-group">
-                    <span class="input-group-addon"><i class="fa fa-inr"></i></span>
-                    <input name="book_original_price" type="text" class="form-control" placeholder="Enter price" required="true">
-                </div> 	 <br><br>
-                Category <br>
-    			<select class="form-control" name="book_category">
-    				<option selected>Choose...</option>
-    				<?php
-    					$query = "SELECT * FROM CATEGORIES WHERE parent_category_id = 0";
+			    <div class="form-group">
+                    <label for="book_category">Author</label>
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="fa fa-user"></i></span>	
+                        <input name="book_author" type="text" class="form-control" placeholder="Enter author name" required="true"> 
+                    </div>
+                </div>
+				<div class="form-group">
+                    <label for="book_category">Original Price</label>
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="fa fa-inr"></i></span>
+                        <input name="book_original_price" type="text" class="form-control" placeholder="Enter price" required="true">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="book_category">Category</label>
+                    <select class="form-control" name="book_category">
+                        <option selected>Choose...</option>
+                        <?php
+                            $query = "SELECT * FROM CATEGORIES WHERE parent_category_id = 0";
 
-    					$query_result_category = mysqli_query($connection,$query);
+                            $query_result_category = mysqli_query($connection,$query);
 
-    					if(!$query_result_category){
-    						die("QUERY FAILED ".mysqli_error($connection));
-    					}
+                            if(!$query_result_category){
+                                die("QUERY FAILED ".mysqli_error($connection));
+                            }
 
-    					while ($row_category = mysqli_fetch_assoc($query_result_category)) {
-    						$category_id = $row_category['category_id'];
-    						$category_name = $row_category['category_name'];
+                            while ($row_category = mysqli_fetch_assoc($query_result_category)) {
+                                $category_id = $row_category['category_id'];
+                                $category_name = $row_category['category_name'];
 
-    						echo "<option value='{$category_id}'>{$category_name}</option>";
+                                echo "<option value='{$category_id}'>{$category_name}</option>";
 
-    					}
-    				?>
-			  	</select> <br><br>
+                            }
+                        ?>
+                    </select> 
+                </div>
+			  	<div class="form-group">
+                    <label for="title">Post Image</label>
+                    <input type="file" class="form-control" name="post_image">		
+                </div>
 			</div>
 		</div>
-		<br>
-		<br>
-		Book description
-		<br>
-		
-		<textarea id="editor" name="book_description" class="form-control" rows="10" placeholder="Enter book's description" style="resize: none;"></textarea><br>
+		<div class="form-group">
+            <label for="book_category">Book Description</label>
+		    <textarea id="editor" name="book_description" class="form-control" rows="10" placeholder="Enter book's description"></textarea>
+        </div>
 	    <input name="submit" type="submit" class="btn btn-primary btn-center" value="Save Details">
 	</form>
 </div>
